0c9be2b143014befa442804096e0d41a126e6208
Archibong Eyo <119775170+Archie202@users.noreply.github.com>
2024-07-16 14:43:15 +0100

    Delete backend directory

--- backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-// backend/routes/auth.js
-const router = require('express').Router();
-const { register, login } = require('../controllers/auth');
-
-router.post('/register', register);
-router.post('/login', login);
-
-module.exports = router;
